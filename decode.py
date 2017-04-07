11bb1d91cea9bb20a3ca23e718762879d1f516b7
Dennis Wu <dennisvwu@gmail.com>
2017-04-06 23:56:07 -0700

    updated
    length count works, decoded message is jumbo :T

--- decode.py
+++ decode.py
@@ -1,63 +1,111 @@
 from PIL import Image
+import binascii, sys
 
 im = Image.open("testImage.png") 
 
 # height & width of image
-x_size = im.size[0]
-y_size = im.size[1]
+width = im.width
+height = im.height
 
-print("The size of the Image is:", im.size[0], " x ", im.size[1])
+print("The size of the Image is:", width, " x ", height)
 
 #container for binary
 binaryArray = []
 
-# scan through image
-for row in range((x_size-1), 0, -1):
-    for col in range((y_size-1), 0, -1):
+# scan through image to get message length
+for row in range((height-1), -1, -1):
+    for col in range((width-1), -1, -1):
     
-        red_value, green_value, blue_value = im.getpixel((row,col))
-    
-        # last 11 pixels reserved for message length
-        if (row == (x_size-1)): 
-		# try: if (y_size-11 <= col <= y_size-1):
-            if ((col == (y_size-1)) | (col == (y_size-2)) | (col == (y_size-3))):
+        # get the r, g, b values from each pixel
+        red_value, green_value, blue_value = im.getpixel((col,row))
+        
+        # grab the lsb of each color value
+        binary_r = (red_value & 1)
+        binary_g = (green_value & 1)
+        binary_b = (blue_value & 1)       
+        
+        # last 11 pixels reserved for message length       
+        if (row == (height-1)):
+               
+            if col in range(width-10, width-1):
+        		
+	            # only grab the least significant bit
+                binaryArray.append(str(binary_r))
+                binaryArray.append(str(binary_g))
+                binaryArray.append(str(binary_b))
             
-                print("Row:", row, "& Col:", col)
-                print("lsb values:", red_value & 1, green_value & 1, blue_value & 1)
-		
-		# only grab the least significant bit
-		binaryArray.append(str(red_value & 1))
-		binaryArray.append(str(green_value & 1))
-		binaryArray.append(str(blue_value & 1))
-            
-            
- #           if ((col == (y_size-4)) | (col == (y_size-5)) | (col == (y_size-6))):
- #            
- #               print("Row:", row, "& Col:", col)
- #               print(red_template & 1, green_template & 1, blue_template & 1)
- #           
- #           if ((col == (y_size-7)) | (col == (y_size-8)) | (col == (y_size-9))):
- #           
- #               print("Row:", row, "& Col:", col)
- #               print(red_template & 1, green_template & 1, blue_template & 1)
- #               
- #           if ((col == (y_size-10)) | (col == (y_size-11))):
- #           
- #               print("Row:", row, "& Col:", col)
- #               print(red_template & 1, green_template & 1, blue_template & 1)
- #	
- #           if (col == (y_size-12)):
- #               
- #               #save only the R and G value for the last 11th pixel
-	
- #  # save all appended values to string using binString = ''.join(binaryArray)
- #  # convert the string to integer with base 2 using int(binString, 2)
+            # only need r & g value on 11th pixel
+            if (col == width-11):
+      
+	           # only grab the least significant bit
+               binaryArray.append(str(binary_r))
+               binaryArray.append(str(binary_g)) 
+           
+# combine all the values & convert to integer               
+binString = ''.join(binaryArray)
+binToint = int(binString, 2)
 
-	
- # # use the integer value as a counter to decode the rest of the image
- # # loop thru string in array [ length / 8 ] times
- # # every 8 binaries append to a string sing: >>> chr(int(binary_string, 2))
- # # output char to terminal
+print("Integer value = ", binToint)
+
+textArray = []
+counter = binToint
+
+# repeat process again to get secret message
+for row in range((height-1), -1, -1):
+    for col in range((width-1), -1, -1):
+    
+        # get the r, g, b values from each pixel
+        red_value, green_value, blue_value = im.getpixel((col,row))
+        
+        # grab the lsb of each color value
+        binary_r = (red_value & 1)
+        binary_g = (green_value & 1)
+        binary_b = (blue_value & 1)       
+        
+        # skip 11 pixels reserved for message length       
+        if (row == (height-1)):  
+                
+            if col in range(width-11, width-1):
+                # do nothing
+                ...
+                
+            # start from 12th pixel
+            if (counter > 0):                  
+               textArray.append(str(binary_r))
+               counter = counter - 1
+            if (counter > 0):
+               textArray.append(str(binary_g))
+               counter = counter - 1
+            if (counter > 0):
+               textArray.append(str(binary_b))
+               counter = counter - 1
+                                      
+        else:
+        
+            # continue to other pixels
+            if (counter > 0):                  
+               textArray.append(str(binary_r))
+               counter = counter - 1
+            if (counter > 0):
+               textArray.append(str(binary_g))
+               counter = counter - 1
+            if (counter > 0):
+               textArray.append(str(binary_b))
+               counter = counter - 1
+               
+            #print("Counter = ", counter)   
+
+           
+# combine all the values & convert to string               
+binMessage = ''.join(textArray)
+
+# convert binary to actual text
+message = ''.join(chr(int(binMessage[i*8: i*8 + 8], 2)) for i in range(len(binMessage) // 8))
+print ("Message = ", message)
+print (binMessage)
+
+      
+
  
 
 	                
